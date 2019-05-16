bdb25907236f7fd340d418ccdf1e29e9022f72c4
Yury Kurlykov <sh1ftr@protonmail.ch>
2019-05-17 05:11:36 +1000

    Bump version in the code
    Everyone forgets about that.

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 const VERSION = Object.freeze({
     MAJOR: 1,
-    MINOR: 0,
-    MICRO: 2,
+    MINOR: 2,
+    MICRO: 0,
     getId: function() {
         return this.MAJOR*37**2 + this.MINOR*37 + this.MICRO;
     },
